877c00b163cf6dd7d0fbba18dc177798bf19d30b
Neel Smith <dnsmith.neel@gmail.com>
2014-05-04 12:58:53 -0400

    Add placeholder for Web class.

---
+++ src/main/groovy/edu/holycross/shot/citedownmgr/Web.groovy
@@ -0,0 +1,41 @@
+package edu.holycross.shot.citedownmgr
+
+import org.pegdown.Extensions
+import org.pegdown.PegDownProcessor
+
+import com.google.common.io.Files
+import org.apache.commons.io.FilenameUtils
+
+
+import edu.harvard.chs.citedown.ast.RootNode
+
+import edu.harvard.chs.citedown.PegDownProcessor
+import edu.harvard.chs.citedown.Extensions
+
+import edu.harvard.chs.citedownutils.MarkdownUtil
+
+import edu.harvard.chs.cite.CiteUrn
+
+
+/**
+* Class for use especially with gradle build, with main method 
+* generating complete bfdocs-compatible source from citedown archive.
+*/
+class Web {
+
+  Web() {
+  }
+
+
+  /** Q&D main method for invocation from build.gradle leanpub task.
+   */
+  public static void main(String [] arg) 
+  throws Exception {
+  }
+
+
+
+
+
+}
+
